606b18439ec6aa5ace2ce588270ef8e8b9eadbc7
Daniel Bodnar <daniel.bodnar@gmail.com>
2020-08-07 14:59:28 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-useradd -m -s /bin/zsh bodnar
-usermod -aG wheel bodnar
 
-pacman -S `https://raw.githubusercontent.com/DanielBodnar/archstrap/master/packages.list`
+pacman -Sy git
+
+git clone https://github.com/DanielBodnar/archstrap.git
+cd archstrap
+chmod +x bootstrap.sh
+./bootstrap.sh
+
+
+#useradd -m -s /bin/zsh bodnar
+#usermod -aG wheel bodnar
+
+#pacman -S `https://raw.githubusercontent.com/DanielBodnar/archstrap/master/packages.list`
