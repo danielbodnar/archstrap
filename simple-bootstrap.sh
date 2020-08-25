6415b6e94920be67af3c77eb1de78c4814b40f2d
Daniel Bodnar <daniel.bodnar@gmail.com>
2020-08-25 10:39:07 -0500

    Create simple-bootstrap.sh

---
+++ simple-bootstrap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+pacman -Sy base base-devel vim git dosfstools mtools fsarchiver gptfdisk haveged hdparm linux-headers linux-firmware mc mkinitcpio vim nfs-utils ntfs-3g openssh rsync sudo zsh ack autoconf automake borg cloud-init cmake crun dua-cli fakechroot fakeroot fd fzf git glances go gpart gparted gzip htop hwinfo iperf3 kcptun jq kexec-tools kubectl linux-zen linux-zen-headers lshw lsof make mtr net-tools nethogs pigz p7zip os-prober pkgconf pv python-pip rustup the_silver_searcher wireguard-tools tmux tmate zip tree buildah grub
